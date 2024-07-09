31e9ce3a7d6bf84aa193dae944f983b5c405828a
unknown <davidpintodev97@gmail.com>
2024-07-09 15:53:04 +0200

    first commit, con .gitignore; dependencias necesarias: spring web, spring dev tools, lombock (opcional); con objeto de facilitar el diseño y creación de la API

---
+++ src/main/java/com/practicaAPI/APISpringBootPractica/ApiSpringBootPracticaApplication.java
@@ -0,0 +1,13 @@
+package com.practicaAPI.APISpringBootPractica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringBootPracticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringBootPracticaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practicaAPI/APISpringBootPractica/ApiSpringBootPracticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practicaAPI.APISpringBootPractica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringBootPracticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
